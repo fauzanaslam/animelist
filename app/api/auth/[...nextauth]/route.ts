77c313074bdcd6cc31aa8947a05a25c242224750
fauzanaslam <fauzanaslamdhani@gmail.com>
2024-05-20 12:44:46 +0700

    test 2

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,14 +1,14 @@
 import NextAuth from "next-auth";
 import githubAuth from "next-auth/providers/github";
 
-export const authOption: any = {
+export const authOption = {
   providers: [
     githubAuth({
       clientId: process.env.GITHUB_CLIENT as string,
       clientSecret: process.env.GITHUB_SECRET as string,
     }),
   ],
-  secret: process.env.NEXTAUTH_SECRET,
+  secret: process.env.NEXTAUTH_SECRET as string,
 };
 
 const handler = NextAuth(authOption);
