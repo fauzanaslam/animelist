e5874f5570ba1e5fc72b66823685b1642bf07793
fauzanaslam <fauzanaslamdhani@gmail.com>
2024-05-20 12:49:34 +0700

    test 4

--- libs/auth-lib.ts
+++ libs/auth-lib.ts
@@ -1,7 +1,7 @@
 import { getServerSession } from "next-auth";
-import { authOption } from "@/app/api/auth/[...nextauth]/route";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 
 export const authUserSession = async () => {
-  const session = await getServerSession(authOption);
+  const session = await getServerSession(authOptions);
   return session?.user;
 };
