afe325a8ce6feb7b71e7582dc10a5d06cb6b8d7f
fauzanaslam <fauzanaslamdhani@gmail.com>
2024-05-20 13:22:11 +0700

    test 6

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,16 +1,14 @@
 import NextAuth from "next-auth";
 import githubAuth from "next-auth/providers/github";
 
-export const authOptions = {
+const handler = NextAuth({
   providers: [
     githubAuth({
-      clientId: process.env.GITHUB_CLIENT,
-      clientSecret: process.env.GITHUB_SECRET,
+      clientId: process.env.GITHUB_CLIENT as string,
+      clientSecret: process.env.GITHUB_SECRET as string,
     }),
   ],
   secret: process.env.NEXTAUTH_SECRET,
-};
-
-const handler = NextAuth(authOptions);
+});
 
 export { handler as GET, handler as POST };
--- libs/auth-lib.ts
+++ libs/auth-lib.ts
@@ -1,6 +1,15 @@
 import { getServerSession } from "next-auth";
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import githubAuth from "next-auth/providers/github";
 
+export const authOptions = {
+  providers: [
+    githubAuth({
+      clientId: process.env.GITHUB_CLIENT as string,
+      clientSecret: process.env.GITHUB_SECRET as string,
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
+};
 export const authUserSession = async () => {
   const session = await getServerSession(authOptions);
   return session?.user;
